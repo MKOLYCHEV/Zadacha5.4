fb27738d0b00f332715fff34ca308cdf4848d33f
Миша Мишин <spawn.rapture-0n@icloud.com>
2021-11-21 09:44:24 +0300

    Добавьте файлы проекта.

---
+++ Zadacha5.4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha5._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 20;
+            int[] array = new int[n];
+            Random random = new Random();
+            Console.Write("Сформированный массив: ");
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = random.Next(-50, 50);
+                Console.Write("{0} ", array[i]);
+            }
+            Console.WriteLine();
+            int sum = 0;
+            for (int i = 1; i < n; i++)
+            {
+                if (i % 2 == 0) continue;
+                if (array[i]>0&&array[i]%2==1)
+                    sum+= 1;
+            }
+            Console.WriteLine("Количество нечетных положительных элементов сформированного массива, стоящих на четных местах, равно {0}.",sum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Zadacha5.4/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Zadacha5.4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Zadacha5.4")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("39610a62-7ccd-41ce-aa1d-a1b0f65e19bd")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
